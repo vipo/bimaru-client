0e7195f93456843a03541ae404dc9e2e6a1429ee
XxDoMaSxX <dragunasdomas@gmail.com>
2022-10-05 23:11:44 +0300

    hint and show funcs -not finished yet

--- src/Lib1.hs
+++ src/Lib1.hs
@@ -11,25 +11,40 @@ import Types
 -- You can change the right hand side as you wish but please
 -- keep the type name as is
 data State =
-  List [State] |
-  Hints Int    |
-  Cols [Int]   | 
-  Rows [Int]
+  Cord [(Int,Int,Char)]
   deriving (Show, Eq)
-
 -- IMPLEMENT
 -- This is very initial state of your program
 emptyState :: State
-emptyState  = List []
+emptyState  = (Cord [])
 
 -- IMPLEMENT
 -- This adds game data to initial state 
 gameStart :: State -> Document -> State
-gameStart state doc = parser doc [state]
+gameStart (Cord state) doc =
+    let cords=makeCords doc ([0],[0]) 
+        tuple= makeTuple cords 'x' []
+    in Cord (tuple++state)
+makeCords :: Document -> ([Int],[Int]) -> ([Int],[Int])
+makeCords (DMap ((str,doc):t)) (col,row) =
+    case str of
+        "number_of_hints"-> makeCords  (DMap t) (col,row)
+        "occupied_cols"-> makeCords  (DMap t) ((makeList doc []),row)
+        "occupied_rows"-> (col,(makeList doc []))
+        _->  error (show str)
 
+makeList  :: Document ->[Int]->[Int]
+makeList (DList [DInteger last]) list= last:list
+makeList (DList (h:t)) list =makeList (DList (t)) ((parser_DInteger h):list)
+makeList dox a = error (show "Bad parrametres")
+
+makeTuple  :: ([Int],[Int])->Char->[(Int,Int,Char)]->[(Int,Int,Char)]
+makeTuple ((hC:tC),(hR:tR)) char tuple = makeTuple (tC,tR) char ((hC,hR,char):tuple)
+makeTuple ([],[]) char tuple = tuple
+makeTuple e1 e2 e3 = error "Bad parametres"
 
 render :: State -> String
-render = show
+render = show 
 
 -- IMPLEMENT
 -- Make check from current state
@@ -45,23 +60,18 @@ toggle l t = emptyState
 -- IMPLEMENT
 -- Adds hint data to the game state
 hint :: State -> Document -> State
-hint l h = emptyState
+hint l h = hintf h l (0,0,'x')
 
-parser :: Document -> [State] -> State
-parser (DMap((a, b):t)) state = 
-    case a of 
-    "number_of_hints" -> parser (DMap t) (Hints   (parserDInteger b):state)
-    "occupied_cols" -> parser (DMap t) (Cols   (colTran b []):state)
-    "occupied_rows" -> parser (DMap t) (Rows   (colTran b []):state)
-    _ -> List state
-parser doc st = List st 
+hintf :: Document -> State ->(Int,Int,Char)-> State
+hintf (DNull) (Cord state2) (x,y,c)= (Cord state2)
+hintf (DMap ((str,doc):t)) (Cord state2) (x,y,c)=
+    case str of
+        "coords"-> hintf doc (Cord state2) (x,y,c)
+        "tail"-> hintf doc (Cord state2) (x,y,c)
+        "head"-> hintf (DMap t) (hintf doc (Cord state2) (x,y,c)) (x,y,c)
+        "col"-> hintf (DMap t) (Cord state2) ((parser_DInteger doc),y,'o')
+        "row"->  Cord ((x,(parser_DInteger doc),c): state2)
 
-
-colTran :: Document -> [Int] -> [Int]
-colTran (DList (skaicius :t)) listas= colTran (DList t) (parserDInteger skaicius:listas) 
-colTran nulis listas = listas
-
-
-parserDInteger :: Document -> Int
-parserDInteger (DInteger a) = a
-parserDInteger a = error "its not DInteger"
+parser_DInteger :: Document -> Int
+parser_DInteger (DInteger a) = a
+parser_DInteger a = error "its not DInteger"
